4ce9cd5d94ef31be62ad785c0c614efd1f3792a8
Eugene Kotsarev <gimmethenick@gmail.com>
2025-02-11 20:53:37 +0300

    feat: add gear-exe theme; remove unused files

--- docs/welcome.md
+++ docs/welcome.md
@@ -6,24 +6,18 @@ sidebar_label: Welcome!
 slug: /
 ---
 
-<img src="/img/title-grey.png"  alt="Gear Tech logo"/>
-
-<div align="center">
-    <h1>Gear.exe - Vision of Decentralized Bridgeless Computing Extension to Ethereum Network</h1>
-</div>
+<div class="text--center">
+<img src="/gear-exe/whitepaper/img/logo-gear.svg" alt="Gear Tech logo" width="212" />
 
 <br />
-
-<div align="center">
-    Nikolay Volf, Andrei Panin
-    <br />
-    <sup><b>Version 0.1</b> | Feb 2025</sup>
-</div>
-
 <br />
 
-<div align="center" valign="middle">
-<sub><sup>© 2025 Gear Technologies, Inc. All Rights Reserved</sup></sub>
-</div>
+# Gear.exe - Vision of Decentralized Bridgeless Computing Extension to Ethereum Network
 
 <br />
+<br />
+
+Nikolay Volf, Andrei Panin <br /> <br />
+**Version 0.1** | Feb 2025
+</div>
+
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,81 +1,185 @@
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
-
-const lightCodeTheme = require('prism-react-renderer').themes.github
-const darkCodeTheme = require('prism-react-renderer').themes.dracula
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+import { themes } from "prism-react-renderer";
 
 const config: Config = {
-  title: 'Gear.exe Whitepaper',
-  url: 'https://gear-exe-wp.gear.foundation/',
-  baseUrl: '/',
-  favicon: '/img/favicon-32x32.png',
-  tagline: 'Vision of Decentralized Bridgeless Computing Extension to Ethereum Network',
-  onBrokenLinks: 'log',
-  onBrokenMarkdownLinks: 'log',
-  onDuplicateRoutes: 'log',
-  organizationName: 'Gear Technologies', // Usually your GitHub org/username.
-  projectName: 'Gear.exe-whitepaper', // Usually your repo name.
-  trailingSlash: false,
+  title: "Gear.exe Whitepaper",
+  tagline:
+    "Vision of Decentralized Bridgeless Computing Extension to Ethereum Network",
+  favicon: "/img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://gear-tech.io",
+
+  // Set the /<baseUrl>/ pathname under which your site is served
+  baseUrl: "/gear-exe/whitepaper",
+
+  onBrokenLinks: "log",
+  onBrokenMarkdownLinks: "log",
+  onDuplicateRoutes: "log",
+
+  organizationName: "Gear Technologies", // Usually your GitHub org/username.
+  projectName: "Gear.exe-whitepaper", // Usually your repo name.
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
 
   presets: [
     [
-      'classic',
-      ({
+      "classic",
+      {
         docs: {
-          path: './docs',
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: "./sidebars.ts",
+          path: "./docs",
+          routeBasePath: "/",
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
-      }  satisfies Preset.Options),
+      } satisfies Preset.Options,
     ],
   ],
 
-  themeConfig:
-    {
-      colorMode: {
-        defaultMode: 'dark',
+  themeConfig: {
+    image: "/img/wp-preview.jpg",
+    colorMode: {
+      defaultMode: "dark",
+      disableSwitch: true,
+    },
+    docs: {
+      sidebar: {
+        autoCollapseCategories: true,
       },
-      image: '/img/wp-preview.jpg',
-      navbar: {
-        title: 'Gear.exe Whitepaper',
-        logo: {
-          alt: 'Gear.exe Whitepaper website',
-          src: 'img/logo-black.svg',
-          srcDark: 'img/logo-white.svg',
+    },
+    navbar: {
+      title: "Gear.exe Whitepaper",
+      logo: {
+        alt: "Gear.exe Whitepaper website",
+        src: "/img/logo.svg",
+        className: "invert dark:invert-0",
+      },
+      items: [
+        {
+          href: "https://gear-tech.io",
+          label: "Gear-tech.io",
+          position: "right",
         },
-        items: [
-          {
-            href: 'https://gear-tech.io',
-            label: 'Gear-tech.io',
-            position: 'right',
-          },
-          {
-            href: 'https://vara.network',
-            label: 'Vara.network',
-            position: 'right',
-          },
-          {
-            href: 'https://github.com/gear-tech/gear',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ['rust', 'toml'],
-      },
-      docs: {
-        sidebar: {
-          autoCollapseCategories: true,
+        {
+          href: "https://vara.network",
+          label: "Vara.network",
+          position: "right",
         },
+        {
+          href: "https://github.com/gear-tech/gear",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Developers",
+          items: [
+            {
+              label: "Wiki",
+              href: "https://wiki.vara.network",
+            },
+            {
+              label: "Contract examples",
+              href: "https://wiki.vara.network/docs/examples/",
+            },
+            {
+              label: "Gear Idea",
+              href: "https://idea.gear-tech.io",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "News",
+              href: "https://gear-tech.io/news",
+            },
+            {
+              label: "Events",
+              href: "https://gear-tech.io/events",
+            },
+            {
+              label: "Workshops",
+              href: "https://gear-tech.io/events?type=79f9ffd4-91d4-4dfd-bab1-c72b74ee596e",
+            },
+          ],
+        },
+        {
+          title: "Company",
+          items: [
+            {
+              label: "About Gear",
+              href: "https://gear-tech.io/events?type=79f9ffd4-91d4-4dfd-bab1-c72b74ee596e#:~:text=COMPANY-,ABOUT%20GEAR,-WHITEPAPER",
+            },
+            {
+              label: "Whitepaper",
+              to: "/",
+            },
+          ],
+        },
+        {
+          title: "Social media",
+          items: [
+            {
+              label: "X.com",
+              href: "https://x.com/gear_techs",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/gear-tech",
+            },
+            {
+              label: "Discord",
+              href: "https://discord.gg/x8ZeSy6S6K",
+            },
+            {
+              label: "Medium",
+              href: "https://medium.com/@gear_techs",
+            },
+            {
+              label: "Telegram",
+              href: "https://t.me/gear_tech",
+            },
+          ],
+        },
+      ],
+      copyright: `© ${new Date().getFullYear()} Gear Technologies, Inc. All Rights Reserved`,
+    },
+    prism: {
+      theme: themes.dracula,
+      additionalLanguages: ["rust", "toml"],
+    },
+    plugins: [
+      async function tailwindPlugin(context, options) {
+        return {
+          name: "docusaurus-tailwindcss",
+          configurePostCss(postcssOptions) {
+            postcssOptions.plugins.push(require("tailwindcss"));
+            postcssOptions.plugins.push(require("autoprefixer"));
+            return postcssOptions;
+          },
+        };
       },
-    } satisfies Preset.ThemeConfig,
-}
+    ],
+    // stylesheets: [
+    //   "https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100..700&display=swap",
+    // ],
+  } satisfies Preset.ThemeConfig,
+};
 
-export default config
+export default config;
